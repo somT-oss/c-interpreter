231c62e8873061a8a99012f2eba3e4e157d053fe
somT-oss <somtochukwuuchegbu@gmail.com>
2024-11-09 14:31:50 +0100

    chore: remove helper comments, they can be found on the learning branch

--- hash_table.c
+++ hash_table.c
@@ -35,6 +35,7 @@ HashTable *create_hashtable() {
         table->buckets[i] = NULL;
     }
     return table;
+}
 
 // ChatGPT Implementation
 void insert(HashTable *table, char *key, char *value) {
@@ -48,12 +49,10 @@ void insert(HashTable *table, char *key, char *value) {
             return;
         }
     } else {
-        HashNode *node = malloc(sizeof(HashNode)); // create a new node
-        node->key = key; // assign the new node a key
-        node->value = value; // assign the new node a value
-        node->next_node = table->buckets[index]; // assign the new node's next_node value to the value of the table's node at that index
-                                                        // by default this will be NULL, if it exists, create a linked list DS connecting the 
-                                                        // existing value at that index with the newly computed hash of the new node.
+        HashNode *node = malloc(sizeof(HashNode)); 
+        node->key = key; 
+        node->value = value;
+        node->next_node = table->buckets[index];
         table->buckets[index] = node; // 
         }
 }
@@ -73,77 +72,30 @@ int search(HashTable *table, char *key) {
     }
 }
 
-
-// Implemented this without looking at GPT LFG🚀🚀
-// This is dogshit and is full of errors
-/* int delete(HashTable *table, char *key) {
-    unsigned int index = hash_function(key);
-    HashNode *node = table->buckets[index];
-
-    if (!node) {
-        printf("key %s not found \n", key);
-        return 1;
-    } else {
-        node->key = NULL;
-        node->value = NULL;
-        node->next_node = NULL;
-        printf("deleted data at index %d", index);
-        return 0;
-    }
-}
-*/
-
 void delete(HashTable *table,char *key) {
     unsigned int index = hash_function(key);
     HashNode *node = table->buckets[index];
     HashNode *prev = NULL;
     
-    /*
-        This block handles traversing the linked list until we get to the node with the same key as the one we want to delete
-    */
     while (node != NULL && strcmp(node->key, key) != 0) {
         prev = node; // update the previous node with the new node
         node = node->next_node; // update the node with the next node
     }
-    
-    /*
-        If after the traversal we don't don't find the node, just return.
-    */
-    if (node == NULL) return; // Key not found
-    
-    /*
-        We enter this block when we have found the key in our node with the same key we want to delete. 
-        Two things can be observed here
-
-        1. If the key is in the first node, update the [head node | current node | node at the ith index] with the value of linked next node in the linked list
-        Linked List Dictionationary: {key1:value1} -> {key2:value2} -> {key3:value3}
-        if the key == key1 and key1 is the first node in the linked list
- 
-        2. If the key is in the middle of the linked list, update the previous node.next value with the node.next value of the key the node is in.
-        Linked List Dictionationary: {key1:value1} -> {key2:value2} -> {key3:value3}
-        if the key == key2 and key2 is the first node in the linked list        
-    */
+    if (node == NULL) {
+        return;
+    } 
     if (prev == NULL) {
-        /*
-            New linked list {key2:value2} -> {key3:value3}    
-        */
         table->buckets[index] = node->next_node;
     } else {
-        /*
-            New linked list {key1:value1} -> {key3:value3} 
-        */
         prev->next_node = node->next_node;
     }
     if (node->key != NULL && node->key != key) {
-        // Free the dynamically allocated key
         free(node->key); 
     }
-    
-    // Free the node itself
     free(node);
 }
 
-// Wrote this by myself without GPT, LFG🚀🚀
+
 void delete_table(HashTable *table) { 
     for (int index = 0; index < HASHTABLE_SIZE; index ++) {
         HashNode *node = table->buckets[index];
@@ -161,28 +113,15 @@ void delete_table(HashTable *table) {
 
 
 void print_hashtable(HashTable *table) {
-    for (int i = 0; i < HASHTABLE_SIZE; i ++) { // loop through the size of the hashtable
-        HashNode *node = table->buckets[i];  // get the node at the ith position
+    for (int i = 0; i < HASHTABLE_SIZE; i ++) { 
+        HashNode *node = table->buckets[i]; 
         if (node == NULL) {
             printf("index: %d \n", i);
         } else {
-            while (node != NULL) { // while the node is not null
-                printf("index: %d -> {%s:%s}\n", i, node->key, node->value); // print the key of the node somto:23
-                node = node->next_node; // set the node to node.next
+            while (node != NULL) {
+                printf("index: %d -> {%s:%s}\n", i, node->key, node->value);
+                node = node->next_node;
             }
         }
     }
-}
-
-
-int main() {
-    printf("This is my custom implementation of a hashtable\n");
-    HashTable *my_hashtable = create_hashtable(); // create a hashtable
-    insert(my_hashtable, "somto", "10"); // insert the values {"somto": "23"}
-    insert(my_hashtable, "chibueze", "9"); // insert the values {"chibueze": "9"}
-    insert(my_hashtable, "chibueze", "9"); // insert the values {"chibueze": "9"}
-    delete(my_hashtable, "chibueze");
-    delete(my_hashtable, "somto");
-    // print_hashtable(my_hashtable);
-    delete_table(my_hashtable);
 }
